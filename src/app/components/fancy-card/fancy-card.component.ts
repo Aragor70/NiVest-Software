39841272a38bc266996669e0f970324072ae6205
Mikolaj Prus <mikis3@o2.pl>
2022-02-17 19:45:21 +0000

    Include assets and looping cards

--- src/app/components/fancy-card/fancy-card.component.ts
+++ src/app/components/fancy-card/fancy-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FancyCardComponent implements OnInit {
 
-  @Input() title: String;
+  @Input() props: any;
 
   constructor() { }
 
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -7,6 +7,55 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   
+  fancyCards: any[] = [
+
+    {
+      title: 'Overview',
+      icon: 'analytics-outline',
+      imgs: [
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+      ]
+    },
+    {
+      title: 'Software development',
+      icon: 'layers-outline',
+      imgs: [
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+      ]
+    },
+    {
+      title: 'Algorithms',
+      icon: 'color-filter-outline',
+      imgs: [
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+      ]
+    },
+    {
+      title: 'About us',
+      icon: 'accessibility-outline',
+      imgs: [
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+        '/assets/fancy/logo.png',
+      ]
+    },
+
+  ]
+
   constructor() {}
 
 }
