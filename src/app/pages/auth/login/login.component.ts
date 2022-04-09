039d2e6c19dc5f24f570dbe4b2e65abc5478cd1f
mikolaj-prus <mikis3@o2.pl>
2022-04-09 14:01:58 +0000

    Client update: login feature

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
   ngOnInit() {}
+
+  public _loginForm: FormGroup;
+  url = '/auth';
+  accessToken : string | null = '';
+
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) { 
+    this.formBuilder.group({
+      email: [
+          '',
+          Validators.compose([Validators.required])
+        ],
+      password: [
+          '',
+          Validators.required
+        ]
+    })
+  }
+
+  login() {
+    this.http.post('/auth', {
+      email: this._loginForm.value.email,
+      password: this._loginForm.value.password,
+    }).subscribe((res: any) => {
+      localStorage.setItem('accessToken', res.accessToken);
+      this.accessToken = localStorage.getItem('accessToken');
+      this.router.navigateByUrl('/dashboard')
+    })
+  }
 
 }
